dc9a641927f2370d7686dd56d6bd7847e67da63f
msbaladev <balamurugan@innesssolutions.com>
2024-05-20 18:05:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import "./index.css"
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='bg-red-500 w-52'>
+      hhhh
+     </div>
+    </>
+  )
+}
+
+export default App
