29b0a99398631f680c6544d5c90c3fc0f218f60d
msbaladev <balamurugan@innesssolutions.com>
2024-05-21 10:02:24 +0530

    components

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import "./index.css"
+import Header from './Components/Header.jsx'
+import Hero from './Components/Hero.jsx'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <>
-     <div className='bg-red-500 w-52'>
-      hhhh
-     </div>
+    <Header />
+    <Hero />
+    
     </>
   )
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    
+  return (
+    <>
+      <h1 className="bg-red-600"> hELOOOO </h1>
+    </>
+  );
+}
+
+export default Header;
